96e062b29f13d30645b7147eaad9c0e27d41709c
Gian Gomes <g_gomees@hotmail.com>
2019-12-10 19:14:17 -0300

    DaoFactory updated

--- src/Model/DAO/DaoFactory.java
+++ src/Model/DAO/DaoFactory.java
@@ -3,7 +3,9 @@ package model.dao;
 import db.DB;
 import model.dao.impl.CategoriaDaoJDBC;
 import model.dao.impl.LoginDaoJDBC;
+import model.dao.impl.PedidoDaoJDBC;
 import model.dao.impl.ProdutoDaoJDBC;
+import model.dao.impl.ProdutoItemDaoJDBC;
 
 public class DaoFactory {
 
@@ -17,5 +19,13 @@ public class DaoFactory {
 	
 	public static ProdutoDao createProdutoDao() {
 		return new ProdutoDaoJDBC(DB.getConnection());
+	}
+	
+	public static PedidoDao createPedidoDao() {
+		return new PedidoDaoJDBC(DB.getConnection());
+	}
+	
+	public static ProdutoItemDao createProdutoItemDao() {
+		return new ProdutoItemDaoJDBC(DB.getConnection());
 	}
 }
