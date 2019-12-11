f0a3e3a5f7dcd1d13b0034a57bf4427f36317bd3
Gian Gomes <g_gomees@hotmail.com>
2019-12-10 23:08:07 -0300

    Pedido and ProdutoItem Controllers implemented

--- src/controller/ProdutoItemController.java
+++ src/controller/ProdutoItemController.java
@@ -1,5 +1,23 @@
 package controller;
 
-public class ProdutoItemController {
+import java.util.List;
 
+import model.dao.DaoFactory;
+import model.dao.ProdutoItemDao;
+import model.entities.ProdutoItem;
+
+public class ProdutoItemController {
+	ProdutoItemDao produtoItemDao = DaoFactory.createProdutoItemDao();
+	
+	public ProdutoItemController() {
+	}
+
+	public boolean insert(ProdutoItem produtoItem) {
+		return produtoItemDao.insert(produtoItem);
+	}
+
+	public List<ProdutoItem> findAllById(Integer id) {
+		return produtoItemDao.findAllById(id);
+	}
+	
 }
